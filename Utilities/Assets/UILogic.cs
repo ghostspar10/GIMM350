db7bc6c4a4b3a600f9aaf0a0437117892164cb4c
ghostspar10 <46792290+ghostspar10@users.noreply.github.com>
2019-09-12 11:36:32 -0600

    Added Utilities
    Added Utilities project

---
+++ Utilities/Assets/UILogic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class UILogic : MonoBehaviour
+{
+    public Text text;
+    public string processedText;
+    public InputField input;
+
+    public void getInput()
+    
+    {
+        processText(input.textComponent.text);
+    }
+    public void displayOutput()
+    {
+        Debug.Log(processedText);
+        double avg = InputHandler.getAverageChars(this.processedText);
+        text.text = avg.ToString();
+    }
+    public void processText(string inS)
+    {
+        Debug.Log("Before input handler: " + inS);
+        //this.processedText = inS;
+        this.processedText = InputHandler.processText(inS);
+        displayOutput();
+    }
+
+}
